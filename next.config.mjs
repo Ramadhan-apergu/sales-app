9ce9a8e8a1bf2b9a0eb46065c97356994a8e617f
karmanto supono <karmantosupono@karmantos-MacBook-Pro.local>
2025-07-23 18:25:52 +0700

    feat: next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import withPWA from '@ducanh2912/next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  // Anda bisa menambahkan opsi lain di sini, seperti `buildExcludes`
+});
+
+export default pwaConfig(nextConfig);
